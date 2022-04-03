feb28118e1ebdc6912e97f52234dee06e8cbefeb
hemangdhanani <hemangdhanani@gmail.com>
2022-04-03 16:33:16 +0530

    saving a model to artifact with time stamp

--- src/training.py
+++ src/training.py
@@ -1,7 +1,9 @@
 # from src.utils.common import read_config
+import os
 from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model
+from utils.model import save_model
 
 import argparse
 
@@ -21,7 +23,15 @@ def training(config_path):
     Validation = (X_cv, y_cv)
     history = model.fit(X_train, y_train,epochs=EPOCHS, validation_data=Validation)
 
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+    
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    
+    model_name = config["artifacts"]["model_name"]
 
+    save_model(model, model_name, model_dir_path)
 
     # print(config)
 
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import time
+import os
 
 def create_model(optimizer_val, loss_func, matrics_method, num_classes):
     LAYERS = [
@@ -13,3 +15,12 @@ def create_model(optimizer_val, loss_func, matrics_method, num_classes):
 
     model_clf.compile(optimizer=optimizer_val, loss=loss_func,metrics=matrics_method)   
     return model_clf # untrained model
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model) 
